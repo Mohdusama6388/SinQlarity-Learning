39fb9fb116b73bb5b53b7d36417c44dda435bef4
Mohd Usama <mohdusama2362004@gmail.com>
2025-06-18 15:19:29 +0530

    programming practice

---
+++ simplecalculator.py
@@ -0,0 +1,24 @@
+num1=int(input("Enter first num :="))
+num2=int(input("Enter second num :="))
+num3=int(input("Enter third num :="))
+choice=int(input("Enter choice 1.sum, "
+"2.Diff, "
+"3.Product,"
+"4.Div :=  "))
+if choice==1 :
+    sum=num1+num2+num3
+    print("sum of three number is :=",sum)
+elif choice==2:
+    diff = num1-num2-num3
+    print("diff of three number is :=",diff)
+elif choice ==3 :
+    product=num1*num2*num3
+    print("product of number is :=", product)
+elif choice ==4 :
+    div =num1/num2/num3    
+print("Division of number is := ",div) 
+
+
+
+
+
