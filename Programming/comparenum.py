c1a03c09674d68e50432561ef1f24f278040c9a3
Mohd Usama <mohdusama2362004@gmail.com>
2025-06-19 14:03:35 +0530

    campare num who is greater

---
+++ Programming/comparenum.py
@@ -0,0 +1,20 @@
+print("enter first number :=")
+n1=int(input())
+print("enter second number :=")
+n2=int(input())
+print("enter third number :=")
+n3=int(input())
+print("enter fourth number :=")
+n4=int(input())
+print("enter fifth number :=")
+n5=int(input())
+if n1>n2 and n1>n3 and n1>n4 and n1>n5 :
+    print(" first num is greater")
+elif n2>n3 and n2>n3 and n2>n5 :
+    print("second num is greater")
+elif n3>n4 and n3>n5 :
+    print("third num is greater")
+elif n4>n5  :
+    print("fourth num is greater")
+else :
+    print("fifth num is greater")    
