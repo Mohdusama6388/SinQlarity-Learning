e7e8e6e872232d1caa9ddd988661680835e02673
Mohd Usama <mohdusama2362004@gmail.com>
2025-06-27 18:36:19 +0530

    Implemented date object method to display date

---
+++ Java Script/date.js
@@ -0,0 +1,8 @@
+let date="2021 07 24 12:19:16 "
+ console.log(date);
+ let dateobj = new Date (date);
+ console.log(dateobj.getmonth());
+ console.log(dateobj.getFullYear());
+ console.log(dateobj.getDate());
+ console.log(dateobj.getDay());
+ console.log(dateobj.getTime());
