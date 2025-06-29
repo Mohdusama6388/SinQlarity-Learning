d11b9e63128e08ae732f9cc82d8145885251dc3d
Mohd Usama <mohdusama2362004@gmail.com>
2025-06-29 22:15:12 +0530

    Implemented basic map object key program

---
+++ ES6 ES2020/mapobjectkey.js
@@ -0,0 +1,7 @@
+const student1={name:"usama",age : 21} 
+const student2={name : "abbasi", age :22}
+ const studentmap= new Map();
+ studentmap.set ("key",12);
+ studentmap.set (student1,'He is topper student')
+studentmap.set(student2,"He is average student")
+console.log(studentmap);
