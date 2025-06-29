c2ed0ce63deacdf702b070f5b08b59764013f383
Mohd Usama <mohdusama2362004@gmail.com>
2025-06-29 11:03:57 +0530

    Implemented Return num of arguments passed program

---
+++ Java Script/JS interview Question/returnnumofargumentspassed.js
@@ -0,0 +1,6 @@
+function argumentlength(...args){
+    return args.length;
+
+}
+const noofretunarguments=argumentlength(1,3,5,6,4,6,7,8,990,7)
+console.log("num of argument passed to function",noofretunarguments);
