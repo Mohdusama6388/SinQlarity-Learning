eab5ba10c4a38fb186e54ef7bc1fb4eb9a50ef8b
Mohd Usama <mohdusama2362004@gmail.com>
2025-06-29 22:32:56 +0530

    Implemented set object function program

---
+++ ES6 ES2020/setobject.js
@@ -0,0 +1,15 @@
+let newset = new Set ();
+newset.add(1);
+newset.add(2);
+newset.add(3);
+newset.add(4);
+newset.add(5);
+newset.add(6);
+newset.add(7);
+newset.add(8);
+console.log(newset);
+
+
+
+
+
