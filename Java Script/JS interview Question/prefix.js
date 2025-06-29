7a07d2124a87500df816449239d481f6c61951b3
Mohd Usama <mohdusama2362004@gmail.com>
2025-06-29 10:50:31 +0530

    Implemented prefix based basic program js

---
+++ Java Script/JS interview Question/prefix.js
@@ -0,0 +1,5 @@
+const addprefixbeforeEveryLine=(strToAddPrefix) => {
+const spilittedstr=strToAddPrefix.split("\n");
+console.log(spilittedstr);
+};
+addprefixbeforeEveryLine('my first line my second line' );
