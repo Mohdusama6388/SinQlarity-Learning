89b73d2228c5fb3706d4b05b856274f79bc402ca
Mohd Usama <mohdusama2362004@gmail.com>
2025-07-04 18:32:02 +0530

    Created basic nodeJS project by using RestAPI and Express

--- Projects/BasicNodeJS/index.js
+++ Projects/BasicNodeJS/index.js
@@ -1,4 +1,4 @@
-const express =require('express');
+const express = require('express');
 const userRoute=require('./routers/user')
 const app =express();
 app.use(express.json());
@@ -7,4 +7,4 @@ app.get('/',(req,res)=>{
     res.send("I am working");
 })
 app.use('/user',userRoute)
-app.listen(3000);
+app.listen(4000);
