af47e1eecf359b26974f0cdc7746829a9eb8a6b2
Mohd Usama <mohdusama2362004@gmail.com>
2025-07-04 11:46:54 +0530

    Added routers module

---
+++ Projects/BasicNodeJS/routers/user.js
@@ -0,0 +1,10 @@
+const express=require('express');
+
+const usercontroller = require('../controllers/user');
+const router = express.Router();
+router.post('/register',usercontroller.register);
+router.post('/get',usercontroller.get);
+router.post('/update',usercontroller.update);
+router.post('/delete',usercontroller.delete);
+
+module.exports=  router;
