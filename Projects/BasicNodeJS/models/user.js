db0af93f356d8981a9ab7d8061b424a0d085033d
Mohd Usama <mohdusama2362004@gmail.com>
2025-07-04 18:27:19 +0530

    Added models module

--- Projects/BasicNodeJS/models/user.js
+++ Projects/BasicNodeJS/models/user.js
@@ -2,8 +2,8 @@ const db=require('../util/Database');
 module.exports.insert=async(userData)=>{
     let id =0;
     try{
-    let query="INSERT INTO users (name,email,password,remark)  VALUES (?,?,?,?)";
-    let result= await db.excute(query,[userData.name,userData.email,userData.password,userData.remark]);
+    let query="INSERT INTO `users` (name,email,password,remark)  VALUES (?,?,?,?)";
+    let result= await db.execute(query,[userData.name,userData.email,userData.password,userData.remark]);
     id=result[0].insertId;
 } catch(error){
     console.log(error);
@@ -13,7 +13,7 @@ return id ;
 module.exports.get=async(userData)=>{
     let retData='';
   try{
-    let query="SElECT * FROM 'users'where id=?";
+    let query="SElECT * FROM `users` WHERE id=?";
     let result=await db.execute(query,[userData.id])
     retData=result[0];
   } catch(error) {
@@ -22,20 +22,19 @@ console.log(error);
   return retData;
 }
 module.exports.update=async(userData)=>{
-    let retData='';
   try{
-    let query="UPDATE `users` SET 'password'=? where id =?";
-    await db.execute(query,[ userData.password,userData.id])
+    let query="UPDATE `users` SET password=? WHERE id =?";
+    await db.execute(query,[userData.password,userData.id])
     return true;
   } catch(error) {
 console.log(error);
   return false ;
   }
 }
-module.exports.delete=async(userData)=>{
+module.exports.delete= async(userData)=>{
   try{
-    let query="DELETE FROM 'users'Where id=?";
-    await db.execute(query,[ userData.password,userData.id])
+    let query="DELETE FROM `users` WHERE id=?";
+    await db.execute(query,[userData.id])
     return true;
   } catch(error) {
 console.log(error);
