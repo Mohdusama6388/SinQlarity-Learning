3fe2113ecd50de08148a2709bbff1f42c98ee6c8
Mohd Usama <mohdusama2362004@gmail.com>
2025-07-05 16:51:22 +0530

    created Basic program using arrow function

---
+++ NodeJS/NodeJS INTERVIEW QUESTION/Arrow_function.js
@@ -0,0 +1,20 @@
+const addtwonumber=(num1,num2) => {
+    return num1+num2;
+}
+const subtwonumber=(num1,num2) => {
+    return num1-num2;
+}
+const dividenum=(num1,num2) => {
+return num1/num2;
+}
+const user_Name=uname => 
+    "HI" +uname
+const result=addtwonumber(5,6);
+console.log(result);
+const result1=subtwonumber(10,6);
+console.log(result1);
+const result2=dividenum(50,6);
+console.log(result2);
+const sname=user_Name("Usama Abbasi") ;  
+console.log(sname);
+
