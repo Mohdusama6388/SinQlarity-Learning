5edf8902232ecfcb20faab4af2b62a26a00fb888
Mohd Usama <mohdusama2362004@gmail.com>
2025-07-05 16:03:27 +0530

    Created object Destructuring basic program

---
+++ NodeJS/NodeJS INTERVIEW QUESTION/object_Destructuring.js
@@ -0,0 +1,8 @@
+const studentmarks=[80,90,59,89,98,45,68,78,89,78];
+ [marks1,marks2,marks3,marks4,marks5 ,...othermarks]= studentmarks;
+console.log("Hindi := ",marks1);
+console.log("English := ",marks2);
+console.log("math marks :=",marks3);
+console.log("science marks := ",marks4);
+console.log("urdu marks  :=",marks5);
+console.log("other activity marks :=",othermarks);
