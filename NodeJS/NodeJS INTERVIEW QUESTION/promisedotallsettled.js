c13da23ac7911be4c8ae9b50ec4a6f71e021b0d6
Mohd Usama <mohdusama2362004@gmail.com>
2025-07-06 17:40:20 +0530

    Created Promisedotallsettled function Basic program

---
+++ NodeJS/NodeJS INTERVIEW QUESTION/promisedotallsettled.js
@@ -0,0 +1,32 @@
+function function1(){
+    return new Promise((rej,res)=>{
+        setTimeout(() => {
+            return res("function1")
+        },2000);
+    
+    })
+        
+    
+}
+function function2(){
+    return new Promise((rej,res)=>{
+        setTimeout(() => {
+            return rej("function2-rejected")
+        },1000);
+    })
+}  
+
+function function3(){
+    return new Promise((rej,res)=>{
+        setTimeout(() => {
+            return res("function3")
+        },1000);
+
+    })
+}  
+  Promise.allSettled ([function1(),function2(),function3()]).then((result)=>{
+    console.log(result)
+  });
+
+      
+        
