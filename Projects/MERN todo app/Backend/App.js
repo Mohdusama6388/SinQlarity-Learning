d6555e8ab7cd4484e5390851a0356a76c17243f4
Mohd Usama <mohdusama2362004@gmail.com>
2025-08-04 11:46:49 +0530

    Added Mern todo Application backend file

---
+++ Projects/MERN todo app/Backend/App.js
@@ -0,0 +1,29 @@
+
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const cors=require("cors");
+const TodoRouter = require('./Routers/todo');
+app.use(cors());
+app.use(express.json());         
+app.use('/', TodoRouter);     
+const uri = "mongodb+srv://mohdusama2362004:ABCD1234@cluster0.iglinrp.mongodb.net/tododb?retryWrites=true&w=majority&appName=Cluster0";
+
+async function startServer() {
+  try {
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected successfully");
+
+    app.listen(3000, () => {
+      console.log(" Server started on port 3000");
+    });
+
+  } catch (err) {
+    console.error(" Failed to connect to MongoDB:", err);
+  }
+}
+
+startServer();
---
+++ Projects/MERN todo app/Backend/model/todo.js
@@ -0,0 +1,13 @@
+
+const mongoose = require("mongoose");
+
+const todoSchema = new mongoose.Schema({
+  userName: {
+    type: String,
+  },
+  mobile: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model("Todo", todoSchema);
